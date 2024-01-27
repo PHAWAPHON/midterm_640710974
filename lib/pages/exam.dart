9b0922bf8f8b5052f1035217eb404e3033ad2603
Phawaphon Phetchuen <phetchuen_p@silpakorn.edu>
2024-01-27 12:10:27 +0700

    ergerg

--- lib/pages/exam.dart
+++ lib/pages/exam.dart
@@ -9,31 +9,32 @@ class TestPage extends StatefulWidget {
 }
 
 class _TestPageState extends State<TestPage> {
-  var _text = "______";
+  var _text = "0";
+  bool check = false;
   Widget buildItem({
     required String text,
-    required String label,
     Color color = Colors.black,
   }) {
     return InkWell(
       onTap: () {
         setState(() {
-          if (_text.length <= 6) {
-            _text = _text.replaceFirst('_', text);
+          if (_text == "0") {
+            _text = text;
+          } else {
+            if (!check) _text += text;
           }
-          //_text2 = label;
         });
       },
       child: Container(
-        width: 80.0,
         padding: const EdgeInsets.all(10.0),
+        width: 300.0,
         child: Column(
           children: [
             Container(
-              width: 60,
-              height: 60,
+              width: 400,
+              height: 100,
               decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey),
+                color: Colors.orange[100],
               ),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -46,11 +47,49 @@ class _TestPageState extends State<TestPage> {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildSymbol({
+    required String text,
+    Color color = Colors.black,
+  }) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          if (_text == "0") {
+            _text = text;
+          } else {
+            if (!check) _text += text;
+          }
+        });
+      },
+      child: Container(
+        padding: const EdgeInsets.all(10.0),
+        width: 300.0,
+        child: Column(
+          children: [
+            Container(
+              width: 400,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.grey[100],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
                   Text(
-                    label,
+                    text,
                     style: GoogleFonts.notoSansThai(
                       color: color,
-                      fontSize: 12.0,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ],
@@ -65,101 +104,239 @@ class _TestPageState extends State<TestPage> {
   @override
   Widget build(BuildContext context) {
     var itemList = [
-      buildItem(text: "1", label: "one", color: Colors.black),
-      buildItem(text: "2", label: "two", color: Colors.black),
-      buildItem(text: "3", label: "three", color: Colors.black),
-      buildItem(text: "4", label: "four", color: Colors.black),
-      buildItem(text: "5", label: "five", color: Colors.black),
-      buildItem(text: "6", label: "six", color: Colors.black),
-      buildItem(text: "7", label: "seven", color: Colors.black),
-      buildItem(text: "8", label: "eight", color: Colors.black),
-      buildItem(text: "9", label: "nine", color: Colors.black),
-      buildItem(text: "0", label: "zero", color: Colors.black),
+      buildItem(text: "7", color: Colors.black),
+      buildItem(text: "8", color: Colors.black),
+      buildItem(text: "9", color: Colors.black),
+      buildItem(text: "\u00F7", color: Colors.black),
+      buildItem(text: "4", color: Colors.black),
+      buildItem(text: "5", color: Colors.black),
+      buildItem(text: "6", color: Colors.black),
+      buildItem(text: "\u00D7", color: Colors.black),
+      buildItem(text: "1", color: Colors.black),
+      buildItem(text: "2", color: Colors.black),
+      buildItem(text: "3", color: Colors.black),
+      buildItem(text: "\u2212", color: Colors.black),
+      buildItem(text: "0", color: Colors.black),
+    ];
+
+    var itemList_p = [
+      buildSymbol(
+        text: "\u00F7",
+      ),
+      buildSymbol(
+        text: "\u00D7",
+      ),
+      buildSymbol(
+        text: "\u2212",
+      ),
     ];
 
     return Scaffold(
       body: Container(
         padding: EdgeInsets.all(50),
         child: Column(
-          
           children: [
-            Column(
-              children: [
-                Icon(
-                  Icons.security,
-                  size: 60,
-                ),
-                SizedBox(height: 8.0),
-                Text(
-                  "PIN LOGIN",
-                  style: GoogleFonts.poppins(fontSize: 20),
-                )
-              ],
-            ),
             Expanded(child: Container()),
             Center(
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   Text(
                     _text,
                     style: GoogleFonts.poppins(
-                        fontSize: 21.0,
+                        fontSize: 60.0,
                         fontWeight: FontWeight.w400,
                         color: Colors.grey),
                   ),
                 ],
               ),
             ),
-            Expanded(child: Container()),
-            for (int i = 0; i < 3; i++)
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  itemList[i * 3],
-                  //SizedBox(width: 4.0),
-                  itemList[i * 3 + 1],
-                  //SizedBox(width: 4.0),
-                  itemList[i * 3 + 2]
-                ],
-              ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      _text = "______";
-                    });
-                  },
-                  child: Icon(
-                    Icons.close,
-                    color: Colors.black,
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _text = "0";
+                      });
+                    },
+                    child: Expanded(
+                      child: Container(
+                        width: 1000,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.blueGrey[50],
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "C",
+                              style: GoogleFonts.notoSansThai(
+                                color: Colors.black,
+                                fontSize: 40.0,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-                SizedBox(width: 10.5),
-                itemList[9],
-                SizedBox(width: 10.5),
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      if (_text.isNotEmpty) {
-                        int lastIndex = _text.lastIndexOf(RegExp('[^_]'));
-                        if (lastIndex != -1) {
-                          _text =
-                              _text.replaceRange(lastIndex, lastIndex + 1, '_');
-                        }
-                      } else {
-                        _text = "______";
-                      }
-                    });
-                  },
-                  child: Icon(
-                    Icons.backspace_outlined,
-                    color: Colors.black,
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _text = _text.substring(0, _text.length - 1);
+                      });
+                    },
+                    child: Expanded(
+                      child: Container(
+                        width: 1000,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.blueGrey[50],
+                        ),
+                        child: Icon(
+                          Icons.backspace_outlined,
+                          color: Colors.black,
+                          size: 40,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ],
+            ),
+            Container(
+              child: Row(
+                children: [
+                  Expanded(child: itemList[0]),
+                  SizedBox(
+                    width: 1,
+                  ),
+                  Expanded(child: itemList[1]),
+                  SizedBox(
+                    width: 1,
+                  ),
+                  Expanded(child: itemList[2]),
+                  SizedBox(
+                    width: 1,
+                  ),
+                  Expanded(child: itemList_p[0]),
+                ],
+              ),
+            ),
+            Container(
+              child: Row(
+                children: [
+                  Expanded(child: itemList[4]),
+                  Expanded(child: itemList[5]),
+                  Expanded(child: itemList[6]),
+                  Expanded(child: itemList_p[1]),
+                ],
+              ),
+            ),
+            Container(
+              child: Row(
+                children: [
+                  Expanded(child: itemList[8]),
+                  Expanded(child: itemList[9]),
+                  Expanded(child: itemList[10]),
+                  Expanded(child: itemList_p[2]),
+                ],
+              ),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _text += "0";
+                      });
+                    },
+                    child: Container(
+                      width: 800,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey[50],
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "0",
+                            style: GoogleFonts.notoSansThai(
+                              color: Colors.black,
+                              fontSize: 35.0,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _text += "+";
+                      });
+                    },
+                    child: Container(
+                      width: 800,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey[50],
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "\u002B",
+                            style: GoogleFonts.notoSansThai(
+                              color: Colors.black,
+                              fontSize: 40.0,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    _text = "0";
+                  });
+                },
+                child: Container(
+                  width: 1200,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: Colors.brown[50],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "=",
+                        style: GoogleFonts.notoSansThai(
+                          color: Colors.black,
+                          fontSize: 50.0,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ),
           ],
         ),
